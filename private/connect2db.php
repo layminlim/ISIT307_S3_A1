5cf01b943ce9289f3d9c5169fbe9f2045873c123
layminlim <laymin.lim@gmail.com>
2021-07-20 19:39:23 +0800

    DB connection

--- private/connect2db.php
+++ private/connect2db.php
@@ -1,10 +1,10 @@
 <?php
-$servername = "127.0.0.1:3306";
+$servername = "127.0.0.1";
 $username = "bikeadmin";
 $password = "Cde3Xsw2Zaq1!@#$";
 $db_name = "test";
 
-$con = mysqli_connect($servername, "root","","test");
+$conn = mysqli_connect($servername, $username, $password, $db_name);
 
 // Check connection
 if (mysqli_connect_errno())
@@ -13,11 +13,13 @@ if (mysqli_connect_errno())
 }
 
 // Perform a query, check for error
-if (!mysqli_query($con,"SELECT * FROM bikes"))
-{
-    echo("Error description: " . mysqli_error($con));
+if ($result = mysqli_query($conn, "SELECT * FROM bikes")) {
+  echo "Returned rows are: " . mysqli_num_rows($result);
+  // Free result set
+  mysqli_free_result($result);
 }
 
-mysqli_close($con);
+
+mysqli_close($conn);
 
 ?>
