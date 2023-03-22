4613d971588c8ef19b6beadbcbb87a12560ec4ce
Caroline Tang <87713884+carolinetangpq@users.noreply.github.com>
2023-03-22 14:22:12 +0800

    Update index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!-- Header -->
+<?php require_once('static/headers/header.php'); ?>
+
+<link rel="stylesheet" href="static/css/index.css">
+
+</head>
+<body>
+  <div class="logo">
+    <img src="logo.png" width="400" alt="Bike Passion Logo"> 
+  </div>
+  
+  <div class="homepage">
+    <div>
+      <a href="buyerpage.php">Buyer</a>
+      /
+      <a href="sellerpage.php">Seller</a>
+    </div>
+  </div>
+</body>
